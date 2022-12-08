db3ad9fad90d03ee1010ce3c65a340f5bce1cb90
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-12-07 21:23:25 -0400

    Refactored the person insert to return void.

--- src/main/java/com/example/springbootmongo/controllers/PersonController.java
+++ src/main/java/com/example/springbootmongo/controllers/PersonController.java
@@ -1,5 +1,6 @@
 package com.example.springbootmongo.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,31 +11,33 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.springbootmongo.entities.Person;
 import com.example.springbootmongo.services.PersonService;
 
 @RestController
-@RequestMapping(value="persons")
+@RequestMapping(value = "persons")
 public class PersonController {
-	
+
 	@Autowired
 	private PersonService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<Person>> findAll() {
 		return ResponseEntity.ok(service.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Person> findById(@PathVariable String id) throws Exception {
 		return ResponseEntity.ok(service.findById(id));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Person> insert(@RequestBody Person person) {
-		return ResponseEntity.ok(service.insert(person));
+	public ResponseEntity<Void> insert(@RequestBody Person person) {
+		person = service.insert(person);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(person.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
-	
 
 }
